de662c545bbf5835f124e54743a4459c17b278a4
dnk89 <dmitrij.kozlovskij@gmail.com>
2021-03-09 23:47:23 +0200

    Declaring a Winner

--- src/index.js
+++ src/index.js
@@ -28,6 +28,8 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i])
+      return;
     squares[i] = this.getNextValue();
     this.setState({
       squares: squares,
@@ -45,7 +47,8 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + this.getNextValue();
+    const winner = calculateWinner(this.state.squares);
+    const status = winner ? 'Winner: ' + winner : 'Next player: ' + this.getNextValue();
 
     return (
       <div>
@@ -84,6 +87,26 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
